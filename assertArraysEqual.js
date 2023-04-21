b1228fc475ac9f294891826cfa2fa2ef57ce9c67
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-04-20 22:20:06 -0400

    Function compares two arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = (array1, array2) => {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return console.log(`✅✅✅ Assertion Passed: Array 1 === Array 2`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: Array 1 !== Array 2`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
