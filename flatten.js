7baa735340907a281ba1308d516dd3321a37cb19
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-04-26 18:28:54 -0400

    Fixing some code in eqArrays

--- flatten.js
+++ flatten.js
@@ -1,4 +1,4 @@
-const eqArrays = (array1, array2) => {
+const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
