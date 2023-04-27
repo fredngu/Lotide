744ae6f7223d2c6765f9daa447a0725d5e4b7f2b
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-04-26 20:19:40 -0400

    Function pushes elements into array until callback function becomes true

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const takeUntil = function(array, callback) {
+  let takenArray = []
+  for (item of array) {
+    if (callback(item)) {
+      return takenArray;
+    } else {
+      takenArray.push(item);
+    }
+  }
+  return takenArray;
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  const inspect = require('util').inspect; 
+  if (eqArrays(array1, array2)) {
+    return console.log(`✅✅✅ Assertion Passed: ${inspect(array1)} === ${inspect(array2)}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${inspect(array1)} !== ${inspect(array2)}`);
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [1,2,5,7,2]);
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", 'been', 'to', 'Hollywood']);
