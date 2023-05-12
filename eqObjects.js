3cc2914aab06543705f1685be2ecd916a5e0dbb7
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-05-12 17:26:21 -0400

    Complete stretch goal - Add recursion in eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,18 +1,22 @@
 const eqArrays = require('./eqArrays');
 
-//Function - eqObjects: checks if both objects are equal 
+//Function - eqObjects: checks if both objects are equal
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   } else {
-    for (let keys of Object.keys(object1)) {
-      if (Array.isArray(object1[keys]) && Array.isArray(object2[keys])) {
+    for (const keys of Object.keys(object1)) {
+      if (Array.isArray(object1[keys] && Array.isArray(object2[keys]))) {
         if (eqArrays(object1[keys], object2[keys]) !== true) {
           return false;
         }
-      } else {
-        if (object1[keys] !== object2[keys]) {
-          return false;
+      } else if (!Array.isArray(object1[keys]) || !Array.isArray(object2[keys])) {
+        if (typeof object1[keys] === 'object' && typeof object2[keys] === 'object') {
+          return eqObjects(object1[keys], object2[keys]);
+        } else {
+          if (object1[keys] !== object2[keys]) {
+            return false;
+          }
         }
       }
     }
