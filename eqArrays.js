86fd962b75a39907fff7d9d3bd72705ead07bc3c
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-05-12 17:27:52 -0400

    Remove trailing space in comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-//Function - eqArrays: checks if both arrays are equal 
+//Function - eqArrays: checks if both arrays are equal
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
